fdd680680d798b77053764c69de81a5125b925df
groot007 <kola.stani@gmail.com>
2017-12-24 10:28:26 +0200

    finished

--- app/js/main.js
+++ app/js/main.js
@@ -7,4 +7,32 @@ $(function(){
 		// fade: true,
   		autoplaySpeed: 2000,
 	});
+
+	$(window).on("load", function(){
+		$(".preloader").delay(500).fadeOut('slow');
+
+		$(".to-left, .to-right").waypoint(function() {
+			var $el = $(this.element),
+				animClass = '';
+			var delay = +$el.attr("data-delay");
+
+			if (($el).hasClass("to-left")){
+				animClass = 'fadeInRight';
+			}else if (($el).hasClass("to-right")){
+				animClass = 'fadeInLeft';
+			}
+
+			if ($(window).width() > 768){
+				setTimeout(function(){
+					$el.addClass(animClass);
+				}, delay);
+			}else{
+				$el.addClass(animClass);
+			}
+
+		  }, { offset: '90%'});
+	});
+
+
+
 });
